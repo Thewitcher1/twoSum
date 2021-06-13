61cd967cbaed757de67374055afc93fc222c1de0
Thewitcher1 <36190319+Thewitcher1@users.noreply.github.com>
2021-06-13 17:24:47 +0200

    Update Two_Sum.cpp

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -42,24 +42,58 @@ int main(){
 }
 
 ---------------------------------SQL Command -------------------------------------------------------
-    
-    creation d'une base de donnée:  create database Client:
-    
-create database client;  -- creation d'une base de donnée
+Primary Key:
 
-create table entrep (     ----- déclaration de tableau 
-  ID int primary key identity,  
+Le clé primaire, permet d’identifier chaque enregistrement dans une table de base de données. Chaque enregistrement de cette
+clé primaire doit être UNIQUE et ne doit pas contenir de valeur NULL.
+La clé primaire est un index, chacune des tables ne peut contenir qu’une seule clé primaire, composée d’une ou plusieurs colonnes.
+L’usage le plus fréquent consiste à créer une colonne numérique qui s’incrémente automatiquement à chaque enregistrement grâce à AUTO_INCREMENT.
+    
+    
+creation d une base de donnée:  create database Client:
+    
+create database client;
+
+create table soc (
+  ID int primary key identity,
+  métier varchar(100) Not Null,
+  pseudo varchar(50) Not null,
+  expertise int,
+  esprit varchar(50),
+  caractère varchar(50)
+);
+
+insert into soc values ('developpeur C/C++','SNE',10, 'excellent', 'smile')
+insert into soc values ('developpeur c#','HYG', 8, 'good', 'nervous')
+insert into soc values ('developpeur Java','kji', 5, 'very good', 'smile')
+insert into soc values ('developpeur NodeJS','JKI', 6, 'excellent', 'smile')
+insert into soc values ('developpeur Javascript','rfg',4, 'good', 'smile')
+
+create table entrep (
+  ID int primary key identity,
   Nom varchar(20) Not Null,
   Prenom varchar(20) Not null,
   Age int,
   Adresse varchar(20),
   Tel varchar(20)
 );
-                                                  ---- remplissage de tableau client
+
 insert into entrep values ('Sahbi','Nebti', 50, 'France', '*******')
 insert into entrep values ('Sahbi1','Nebti1', 40, 'France1', '1*******')
 insert into entrep values ('Sahbi2','Nebti2', 30, 'France2', '2*******')
 insert into entrep values ('Sahbi3','Nebti3', 20, 'France3', '3*******')
 insert into entrep values ('Sahbi4','Nebti4', 10, 'France4', '4*******')
 
-delete from entrep where id =1;    -----supprimer toute une ligne de tableau client 
+insert into entrep values ('sahbi','Nebti', 50, 'France','*******') --- insertion d'un nouveau ligne pour le test (qui sera supprimé aprés)
+select distinct Nom,Prenom,Age,Adresse,Tel from entrep;  --- pour éviter les champs doublons
+delete from entrep where id=6; --- supprimer toute une ligne de du tableau entrep 
+select métier from soc where id=1; --- selectionner la colonne demandée du nom_tableau ou le id vaut 1.
+    
+ select * from soc where expertise <11 and caractère='smile'; ---------- and et or with where 
+ select métier from soc where id =1 or id =2 or id=5;          ---------- and et or with where 
+ select métier from soc where id in(1,2,5);                    ---------- Opérateur in 
+
+
+select * from soc;
+select * from entrep;
+
